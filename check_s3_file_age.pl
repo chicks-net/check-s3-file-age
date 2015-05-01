d32da87737aef7e8fe0eb7abfb0f66f0ab5076b4
Christopher Hicks <chicks.net@gmail.com>
2015-05-01 22:26:19 +0000

    nagios plugin boilerplate :fork_and_knife:

---
+++ check_s3_file_age.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use English;
+use Getopt::Long;
+use utils qw (%ERRORS &print_revision &support);
+use vars qw($PROGNAME);
+
+sub print_help ();
+sub print_usage ();
