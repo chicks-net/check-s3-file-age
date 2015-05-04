c97f4ba69a78ffe841ff29675875a7a751957996
Christopher Hicks <chicks.net@gmail.com>
2015-05-04 06:39:06 +0000

    check age :calendar:

--- check_s3_file_age.pl
+++ check_s3_file_age.pl
@@ -34,9 +34,9 @@ GetOptions(
 	"secretkey|s=s"	=> \$opt_s, "AWS-secret-key"	=> \$opt_s,
 	"bucket|n=s"	=> \$opt_n, "S3-bucket-name"	=> \$opt_n,
 	"f=s"		=> \$opt_f, "file"		=> \$opt_f,
-	"w=f"		=> \$opt_w, "warning-age=f"	=> \$opt_w,
+	"w=s"		=> \$opt_w, "warning-age=f"	=> \$opt_w,
 	"W=f"		=> \$opt_W, "warning-size=f"	=> \$opt_W,
-	"c=f"		=> \$opt_c, "critical-age=f"	=> \$opt_c,
+	"c=s"		=> \$opt_c, "critical-age=f"	=> \$opt_c,
 	"C=f"		=> \$opt_C, "critical-size=f"	=> \$opt_C
 );
 
@@ -80,6 +80,7 @@ unless (defined $s3_file_name) {
 # actually check something
 #
 my $result = 'OK';
+my $nagios_text = '';
 
 my $s3 = Net::Amazon::S3->new(
     {
@@ -99,8 +100,6 @@ unless (defined $bucket) {
 	exit $ERRORS{$result};
 }
 
-#print "DEBUG using " . $bucket->bucket . "\n";
-
 # check file
 my $file_response = $bucket->get_key($s3_file_name);
 unless ($file_response) {
@@ -110,30 +109,53 @@ unless ($file_response) {
 	exit $ERRORS{$result};
 }
 
-#print Dumper($file_response);
-#print "KEYS:", join(', ', keys %$file_response), "\n";
-#foreach my $fld (qw(date last-modified client-date content_length )) {
-#	my $value = $file_response->{$fld} || 'undef';
-#	print "$fld\t= $value\n";
-#}
-
+# check file size
 my $size = $file_response->{content_length};
 #print "size=$size\ncrit=$opt_C\nwarn=$opt_W\n";
 
 if ($opt_C and $size < $opt_C) {
-#	warn "size crit";
+	$nagios_text = "SIZE crit $size<$opt_C";
 	$result = 'CRITICAL';
 } elsif ($opt_W and $size < $opt_W) {
-#	warn "size warn";
+	$nagios_text = "SIZE warn $size<$opt_W";
 	$result = 'WARNING';
+} else {
+	my $meg = sprintf("%.2f", $size / (1024*1024) );
+	$nagios_text = "size ok ${meg}MB";
+}
+$nagios_text .= ', ';
+
+# check file age
+if ($opt_c or $opt_w) {
+	my $age_crit = read_seconds($opt_c);
+	my $age_warn = read_seconds($opt_w);
+
+	my $age = $file_response->{'last-modified'};
+#	print "age=$age\n";
+	my $age_time = str2time($age);
+#	print "age_time=$age_time\n";
+	my $now = time;
+	my $diff_secs = $now - $age_time;
+#	print "diff_secs=$diff_secs\n";
+#	print "age_crit=$age_crit\n";
+#	print "age_warn=$age_warn\n";
+
+	if ($diff_secs > $age_crit) {
+		$nagios_text .= "AGE crit $diff_secs>$age_crit";
+		$result = 'CRITICAL';
+	} elsif ($diff_secs > $age_warn) {
+		$nagios_text .= "AGE warn $diff_secs>$age_warn";
+		$result = 'WARNING';
+	} else {
+		$nagios_text .= "age ok $diff_secs secs";
+	}
+} else {
+	$nagios_text .= "age ignored";
 }
 
-my $age = $file_response->{'last-modified'};
-print "age=$age\n";
-
-print "exit with $result\n";
-
 # last exit
+print "$PRENAG $nagios_text\n";
+#print "exit with $result\n";
 exit $ERRORS{$result};
 
 #
@@ -159,4 +181,29 @@ sub print_help () {
 	print "  <secret-key> is optional if you have the nvironment variables AWS_SECRET_KEY defined\n";
 	print "\n";
 	support();
+}
+
+sub read_seconds {
+	my ($raw_seconds) = @_;
+
+	$raw_seconds = 0 unless defined $raw_seconds;
+
+	my %factor = (
+		m => 60,		# minute
+		h => 60*60,		# hour
+		d => 60*60*24,		# day
+		w => 60*60*24*7,	# week
+	);
+
+	my $actual_seconds;
+	if ($raw_seconds =~/^\d+$/) {
+		$actual_seconds = $raw_seconds;
+	} elsif ($raw_seconds =~/^(\d+)([dhwm])$/) {
+		$actual_seconds = $1 * $factor{$2};
+	} else {
+		warn "parse fail $raw_seconds";
+		$actual_seconds = 0;
+	}
+
+	return $actual_seconds;
 }
